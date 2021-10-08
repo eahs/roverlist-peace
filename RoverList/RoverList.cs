6e4875b8db2f9d015ae42807c862345ba1ff4396
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2021-10-08 09:27:00 -0400

    Fixed add

--- RoverList/RoverList.cs
+++ RoverList/RoverList.cs
@@ -24,6 +24,7 @@ namespace RoverList
             if (head == null)
             {
                 head = newNode;
+                current = newNode;
             }
             else
             {
