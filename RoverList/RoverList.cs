03d6d4a796207dfe5da33503b61ea29fcaa30b31
Karolina Kotlarz <kotlarzkarolina16@gmail.com>
2021-10-08 09:24:21 -0400

    Add methods - 1st

--- RoverList/RoverList.cs
+++ RoverList/RoverList.cs
@@ -9,27 +9,40 @@ namespace RoverList
     public class RoverList<T> : RoverListBase<T>
     {
         // Add any variables you need here
+        private int count;
         
         public RoverList ()
         {
-
+            count = 0;
         }
 
-        public override int Count => throw new NotImplementedException();
+        public override int Count => count;
 
         public override void Add(T data)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                current.Next = newNode;
+                current = newNode;
+            }
+            count++;
         }
 
         public override void Add(int Position, T data)
         {
+            Node newNode = new Node(data);
+            
             throw new NotImplementedException();
         }
 
         public override void Clear()
         {
-            throw new NotImplementedException();
+            head = null;
         }
 
         public override T ElementAt(int Position)
